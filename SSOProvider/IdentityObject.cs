c6ec70b69b9163ec9dc7786168280c5c0b9dc075
Tejaswi Bishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2023-07-07 00:08:36 +0530

    Identity Provider Interface and objects defined

---
+++ SSOProvider/IdentityObject.cs
@@ -0,0 +1,13 @@
+﻿namespace IdentityProvider
+{
+    public class IdentityObject
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Username { get; set; }
+        public string? MainField { get; set; }
+        public string? PictureURL { get; set; }
+        public string? Token { get; set; }
+    }
+}
---
+++ SSOProvider/IdentityProvider.cs
@@ -0,0 +1,19 @@
+﻿namespace IdentityProvider
+{
+    public class IdentityObject
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Username { get; set; }
+        public string? MainField { get; set; }
+        public string? PictureURL { get; set; }
+        public string? Token { get; set; }
+    }
+    public interface IIdentityProvider
+    {
+        string Name { get; }
+        IdentityObject? ProvideIdentity();
+        void Configure(string? config);
+    }
+}
