ea45edf635006bfdd16fef85cc9a5abe9112cba7
Tejaswi Bishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2023-07-13 01:11:50 +0530

    Added Documentation for IIdentityProvider and implemented important methods in GoogleIdentityProvider.

--- IdentityProvider/IIdentityProvider.cs
+++ IdentityProvider/IIdentityProvider.cs
@@ -6,10 +6,37 @@ namespace IdentityProvider
     /// </summary>
     public interface IIdentityProvider
     {
+        /// <summary>
+        /// Name of the IdentityProvider as per configuration.
+        /// It will help in identification of the provider.
+        /// </summary>
         string Name { get; }
-        IDictionary<string, string>? CreateAuthorizationRequest();
+        /// <summary>
+        /// Creates a new authorization request with the given parameters and as per configuration.
+        /// </summary>
+        /// <param name="signup"></param>
+        /// <returns>A collection of Key-Value pairs representing different parameters of the auth request.</returns>
+        IDictionary<string, string>? CreateAuthorizationRequest(bool signup);
+        /// <summary>
+        /// This method exchanges the authorization code for the identity information.
+        /// To be used for sign-up when we need additional information to pre-fill user profile.
+        /// </summary>
+        /// <param name="code">The Authorization Code</param>
+        /// <returns>An IdentityObject object which contains all the necessary identity information.</returns>
         IdentityObject? ExchangeCodeForIdentityInfo(string? code);
+        /// <summary>
+        /// This method exchanges the authorization code for the identity.
+        /// It does not return additional information, just the parameters that can authenticate the user.
+        /// </summary>
+        /// <param name="code">The Authorization Code</param>
+        /// <returns>The string representation of chosen identity parameter.</returns>
         string ExchangeCodeForIdentity(string? code);
+        /// <summary>
+        /// This method configures the IdentityProvider with the configuration provided in the configuration file through <paramref name="config"/>.
+        /// </summary>
+        /// <param name="name">Name of the Identity Provider.</param>
+        /// <param name="config">Parameters from the config file.</param>
+        /// <seealso cref="IConfigurationSection" href="https://learn.microsoft.com/en-us/dotnet/api/microsoft.extensions.configuration.iconfigurationsection?view=dotnet-plat-ext-6.0"/>
         void Configure(string name, IConfigurationSection config);
     }
 }
--- IdentityProvider/OAuthIdentityProvider.cs
+++ IdentityProvider/OAuthIdentityProvider.cs
@@ -1,18 +1,88 @@
-﻿using RestSharp;
+﻿using Microsoft.Extensions.Configuration;
+using RestSharp;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IdentityProvider
 {
+    /// <summary>
+    /// Provides Google OAuth2.0/OIDC Identity Services
+    /// </summary>
     public class GoogleIdentityProvider: IIdentityProvider
     {
-        public string Name { get; set; }
-        public string ClientID { get; set; }
-        public string ClientSecret { get; set; }
-        RestClient Client { get; set; }
-        
+        public string Name { get; private set; }
+        private string? ClientID { get; set; }
+        private string? ClientSecret { get; set; }
+        private string? RedirectURL { get; set; }
+        private RestClient? RedirectClient;
+        private RestClient? ExchangeClient;
+        private string? SignUpScope { get; set; }
+        private string? SignInScope { get; set; }
+        private bool Configured { get; set; } = false;
+        public IDictionary<string, string>? CreateAuthorizationRequest(bool signup = false)
+        {
+            if (!Configured)
+            {
+                throw new InvalidOperationException("Identity Provider not configured.");
+            }
+            var scope = signup ? SignUpScope : SignInScope;
+            var request = new RestRequest("", Method.Get);
+            string state = Name + ":" + Guid.NewGuid().ToString();
+            request.AddParameter("client_id", ClientID);
+            request.AddParameter("redirect_uri", RedirectURL);
+            request.AddParameter("scope", scope);
+            request.AddParameter("response_type", "code");
+            request.AddParameter("access_type", "online");
+            request.AddParameter("include_granted_scopes", "true");
+            request.AddParameter("state", state);
+            request.AddParameter("prompt", "consent");
+            string redirUrl = RedirectClient.BuildUri(request).AbsoluteUri;
+            return new Dictionary<string, string> { { "URL", redirUrl}, { "State", state } };
+        }
+        private string GetIDToken(string code)
+        {
+            if (!Configured)
+            {
+                throw new InvalidOperationException("Identity Provider not configured.");
+            }
+            RestRequest request = new("", Method.Post);
+            request.AddParameter("code", code);
+            request.AddParameter("client_id", ClientID);
+            request.AddParameter("client_secret", ClientSecret);
+            request.AddParameter("redirect_uri", RedirectURL);
+            request.AddParameter("grant_type", "authorization_code");
+            var response = ExchangeClient?.Execute(request);
+            if (response?.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new HttpRequestException("Failed to exchange code for token.");
+            }
+            try
+            {
+                var resp = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(json: response?.Content);
+                if (resp == null || !resp.ContainsKey("id_token")) throw new Exception("Failed to deserialize response and fetch ID Token");
+                return resp["id_token"];
+            }
+            catch (Exception ex)
+            {
+                throw new DeserializationException(response, ex);
+            }
+            return "";
+        }
+        public GoogleIdentityProvider()
+        {
+            Name = "NC";
+        }
+        public void Configure(string name, IConfigurationSection config)
+        {
+            if (Configured) return;
+            Name = name;
+            ClientID = config["ClientID"];
+            ClientSecret = config["ClientSecret"];
+            RedirectURL = config["RedirectURL"];
+            RedirectClient = new RestClient("https://accounts.google.com/o/oauth2/v2/auth");
+            ExchangeClient = new RestClient("https://oauth2.googleapis.com/token");
+            SignInScope = config["SignInScope"];
+            SignUpScope = config["SignUpScope"];
+            Configured = true;
+        }
     }
 }
