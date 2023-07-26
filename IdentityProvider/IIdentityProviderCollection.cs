f89b3f709a00ba941a2e927927fd7a008dca91a0
Tejaswi Bishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2023-07-26 22:42:10 +0530

    Documentation and implementation of IdentityProviderCollection

--- IdentityProvider/IIdentityProviderCollection.cs
+++ IdentityProvider/IIdentityProviderCollection.cs
@@ -13,16 +13,46 @@ namespace IdentityProvider
     /// </summary>
     public interface IIdentityProviderCollection
     {
+        /// <summary>
+        /// Returns the Identity Provider with the given name.
+        /// </summary>
+        /// <param name="name">Indexer for IIdentityProvider</param>
+        /// <returns>Identity Provider with given name</returns>
         IIdentityProvider? this[string name] { get; }
+        /// <summary>
+        /// Builds the collection of Identity Providers from the given configuration.
+        /// </summary>
+        /// <param name="config">The configuration using which the collection has to be created.</param>
         void Build(IConfigurationSection config);
+        /// <summary>
+        /// Add a new Identity Provider to the collection.
+        /// </summary>
+        /// <param name="provider">The Identity Provider which is to be added.</param>
         void Add(IIdentityProvider provider);
+        /// <summary>
+        /// Add a new Identity Provider to the collection from the given configuration.
+        /// </summary>
+        /// <param name="config">Configuration from which the Identity Provider will be created.</param>
         void AddFromConfiguration(IConfigurationSection config);
+        /// <summary>
+        /// Remove the given Identity Provider from the collection.
+        /// </summary>
+        /// <param name="provider">The Identity Provider which has to be removed.</param>
         void Remove(IIdentityProvider provider);
+        /// <summary>
+        /// Remove the Identity Provider with the given key from the collection.
+        /// </summary>
+        /// <param name="key">Name/Key of the Identity Provider that has to be removed.</param>
         void RemoveKey(string key);
     }
+    /// <summary>
+    /// Collection of Identity Providers. Referenced by controllers to get the correct Identity Provider.
+    /// </summary>
+    /// <remarks>A reference/default implementation of IIdentityProviderCollection</remarks>
     public class IdentityProviderCollection: IIdentityProviderCollection
     {
-        private IDictionary<string, IIdentityProvider> _identityProviders;
+        private readonly IDictionary<string, IIdentityProvider> _identityProviders;
+        private readonly IIdentityProviderFactory _identityProviderFactory;
         public IIdentityProvider? this[string name]
         {
             get {
@@ -30,9 +60,66 @@ namespace IdentityProvider
                 else return null;
             }
         }
-        public IdentityProviderCollection()
+        /// <summary>
+        /// The constructor for IdentityProviderCollection.
+        /// </summary>
+        /// <param name="identityProviderFactory">The Identity Provider Factory that is to be used. For DI purpose.</param>
+        public IdentityProviderCollection(IIdentityProviderFactory identityProviderFactory)
         {
             _identityProviders = new Dictionary<string, IIdentityProvider>();
+            _identityProviderFactory = identityProviderFactory;
+        }
+        public void Build(IConfigurationSection config)
+        {
+            //throw new NotImplementedException("Build method not implemented");
+            foreach (var section in config.GetChildren())
+            {
+                foreach(var subsection in section.GetChildren())
+                {
+                    var identityProvider = _identityProviderFactory.GetIdentityProvider(subsection);
+                    if (identityProvider != null)
+                    {
+                        _identityProviders.Add(identityProvider.Name, identityProvider);
+                    }
+                }
+            }
+        }
+        public void Add(IIdentityProvider identityProvider)
+        {
+            //throw new NotImplementedException("Add method not implemented");
+            if (!_identityProviders.ContainsKey(identityProvider.Name))
+            {
+                _identityProviders.Add(identityProvider.Name, identityProvider);
+            }
+            else throw new Exception("Identity Provider already exists");
+        }
+        public void AddFromConfiguration(IConfigurationSection config)
+        {
+            //throw new NotImplementedException("AddFromConfiguration method not implemented");
+            var identityProvider = _identityProviderFactory.GetIdentityProvider(config);
+            if (identityProvider != null && !_identityProviders.ContainsKey(identityProvider.Name))
+            {
+                _identityProviders.Add(identityProvider.Name, identityProvider);
+            }
+            else if (identityProvider != null) throw new Exception("Identity Provider already exists");
+        }
+        public void Remove(IIdentityProvider provider)
+        {
+            //throw new NotImplementedException("Remove method not implemented");
+            if (_identityProviders.ContainsKey(identityProvider.Name))
+            {
+                _identityProviders.Remove(identityProvider.Name);
+            }
+            else throw new Exception("Identity Provider does not exist");
+        }
+        public void RemoveKey(string key)
+        {
+            //throw new NotImplementedException("RemoveKey method not implemented");
+            if (_identityProviders.ContainsKey(key))
+            {
+                _identityProviders.Remove(key);
+            }
+            else throw new Exception("Identity Provider does not exist");
         }
     }
 }
---
+++ IdentityProvider/IIdentityProviderFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityProvider
+{
+    public class IdentitityProviderMetadata
+    {
+        string Name { get; set; }
+        string Message { get; set; }
+        string Icon { get; set; }
+    }
+    public interface IIdentityProviderFactory
+    {
+        IIdentityProvider? GetIdentityProvider(IConfigurationSection config);
+        IdentitityProviderMetadata GetMetadata(IConfigurationSection config);
+    }
+    public class IdentityProviderFactory: IIdentityProviderFactory
+    {
+        public IIdentityProvider? GetIdentityProvider(IConfigurationSection config)
+        {
+            throw new NotImplementedException("GetIdentityProvider method not implemented");
+        }
+        public IdentitityProviderMetadata GetMetadata(IConfigurationSection config)
+        {
+            throw new NotImplementedException("GetMetadata method not implemented");
+        }
+    }
+}
