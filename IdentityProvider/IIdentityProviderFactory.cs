2bef9d9d914ce21978ccbfbecc1e85ee95a9156c
Tejaswi Bishnoi <62018605+TejaswiBishnoi@users.noreply.github.com>
2023-07-27 23:06:04 +0530

    Some more documentation

--- IdentityProvider/IIdentityProviderFactory.cs
+++ IdentityProvider/IIdentityProviderFactory.cs
@@ -7,17 +7,46 @@ using System.Threading.Tasks;
 
 namespace IdentityProvider
 {
+    /// <summary>
+    /// Metadata for Identity Provider
+    /// </summary>
     public class IdentitityProviderMetadata
     {
+        /// <summary>
+        /// The name associated with the IdentityProvider
+        /// </summary>
         string Name { get; set; }
+        /// <summary>
+        /// Message to be displayed with Identity Provider (e.g. "Sign in with Google")
+        /// </summary>
         string Message { get; set; }
+        /// <summary>
+        /// Icon to be displayed with Identity Provider (e.g. Logo of Microsoft)
+        /// </summary>
         string Icon { get; set; }
     }
+    /// <summary>
+    /// Interface for Identity Provider Factory
+    /// </summary>
     public interface IIdentityProviderFactory
     {
+        /// <summary>
+        /// Instantiates and fetch the Identity Provider with the given configuration.
+        /// </summary>
+        /// <param name="config">The Configuration for Identity Provider</param>
+        /// <returns>The requested Identity Provider.</returns>
         IIdentityProvider? GetIdentityProvider(IConfigurationSection config);
+        /// <summary>
+        /// Fetches the Identity Provider Metadata with the given configuration.
+        /// </summary>
+        /// <param name="config">The configuration from which metadata is built.</param>
+        /// <returns>The Metadata</returns>
         IdentitityProviderMetadata GetMetadata(IConfigurationSection config);
     }
+    /// <summary>
+    /// Reference/Default implementation of IIdentityProviderFactory
+    /// This will be used for instantiating Identity Providers.
+    /// </summary>
     public class IdentityProviderFactory: IIdentityProviderFactory
     {
         public IIdentityProvider? GetIdentityProvider(IConfigurationSection config)
